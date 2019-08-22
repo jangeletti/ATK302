ad7236fb5649d26e591e4849271960d45d0ebe1f
James <jrange1@ilstu.edu>
2019-08-22 11:07:22 -0500

    uploading

--- p5/circle/sketch.js
+++ p5/circle/sketch.js
@@ -6,5 +6,9 @@ function setup() {
 function draw() {
   // put drawing code here
 
-  ellipse(mouseX, mouseY, 10, 10) ;
+  rect(mouseX, mouseY, 10, 10);
+}
+
+function draw () {
+  rect (100, 100, 40, 300)
 }
