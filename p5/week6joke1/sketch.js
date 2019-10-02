a1b3775f4bcf9ef100ec7c25edc9d50f255279d7
James <jrange1@ilstu.edu>
2019-10-01 23:29:52 -0500

    joke 1

--- p5/week6joke1/sketch.js
+++ p5/week6joke1/sketch.js
@@ -10,11 +10,23 @@ function draw () {
   switch(myState){
   case 0:
   fill('black');
+  textAlign(CENTER);
+  textSize(32);
   text('How do you comfort a Javascript bug?', width/2, height/2);
   break;
 
   case 1:
-  text('You console it.', width/2, height/2);
+  textAlign(CENTER);
+  textSize(64);
+  text('You', width/2 - 175, height/2 - 100);
+  fill('yellow');
+  textStyle(BOLD);
+  textSize(128);
+  text('console', width/2, height/2);
+  textStyle(NORMAL);
+  textSize(64);
+  fill('black');
+  text('it.', width/2 + 150, height/2 + 75);
   break;
 }
 }
