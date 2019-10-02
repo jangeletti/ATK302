cd211dd43e7f4adc64e62018618178428c030f1c
James <jrange1@ilstu.edu>
2019-10-02 00:37:31 -0500

    done

--- p5/week6multaudio/sketch.js
+++ p5/week6multaudio/sketch.js
@@ -1,25 +1,31 @@
 var song1, song2, song3;
+var image1, image2, image2;
 var myState = 0;
 
 
 function preload() {
-  song1 = loadSound('assets/donkey.mp3');
-  song2 = loadSound('assets/skate.mp3');
-  song3 = loadSound('assets/thrill.mp3');
+  song1 = loadSound('assets/arianagrande.mp3');
+  song2 = loadSound('assets/lizzo.mp3');
+  song3 = loadSound('assets/billieeilish.mp3');
+  image1 = loadImage('assets/image.png');
+  image2 = loadImage('assets/photoshoot012.jpg');
+  image3 = loadImage('assets/red.jpg');
 
+  // set each song to loop and then immediately pause it so it doesn't play
+
+  song1.loop();
+  song1.stop();
+  song2.loop();
+  song2.stop();
+  song3.loop();
+  song3.stop();
 
 }
 
 function setup() {
 
-  createCanvas(200, 200);
-  // set each song to loop and then immediately pause it so it doesn't play
-    song1.loop();
-    song1.stop();
-    song2.loop();
-    song2.stop();
-    song3.loop();
-    song3.stop();
+  createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
@@ -35,7 +41,15 @@ function draw() {
     case 1:
     // hang out in here, put some lyrics or pictures or something while the song plays.
     // put pictures and shapes in here
-
+    imageMode(CENTER);
+    image(image1, width/2, height/2, 400, 400);
+    fill('white');
+    textAlign(CENTER);
+    textSize(32);
+    text('Boyfriend - Ariana Grande & Social House', width/2, height/2);
+    textSize(12);
+    fill(150);
+    text('Click anywhere to change songs', width/2, height/2+50);
       break;  // we would only leave state 1 with a mouseClick
 
 
@@ -46,6 +60,14 @@ function draw() {
       break;
 
     case 3:
+    image(image2, width/2, height/2, 400, 400);
+    fill('red');
+    textAlign(CENTER);
+    textSize(32);
+    text('Truth Hurts - Lizzo (BPM Supreme Jump Off Edit)', width/2, height/2);
+    textSize(12);
+    fill(150);
+    text('Click anywhere to change songs', width/2, height/2+50);
       break;
 
     case 4:
@@ -54,6 +76,14 @@ function draw() {
       break;
 
     case 5:
+    image(image3, width/2, height/2, 432, 288);
+    fill('white');
+    textAlign(CENTER);
+    textSize(32);
+    text('Bad Guy vs Stop It - Billie Eilish vs Fisher vs Cheyenne Giles (All Gold VIP Bootleg)', width/2, height/2);
+    textSize(12);
+    fill(150);
+    text('Click anywhere to change songs', width/2, height/2+50);
       break;
 
   }
